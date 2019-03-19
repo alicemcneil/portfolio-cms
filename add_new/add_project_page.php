06b19f3fb40a06fe1dddc5811248a89b2357ddd8
Alice <alice@rocketmakers.com>
2019-03-19 11:51:26 +0000

    made a 'create new project' (no function)

---
+++ add_new/add_project_page.php
@@ -0,0 +1,13 @@
+<?php
+echo '<html lang="en"></html> <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="add_proj.css">
+
+<form class="upload">
+<h1>Create new Project</h1>
+<h3>Project Name: </h3><input type="text" name="project_title">
+<h3>Image URL: </h3><input type="text" name="img_url">
+<h3>Site URL: </h3><input type="text" name="project_title">
+<br><br>
+<input type="submit" value="Add to Portfolio">
+</form>';
+
