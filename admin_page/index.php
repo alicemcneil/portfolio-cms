4c6a9cbae9f0eb31319425346732a5246ff462b0
Alice <alice@rocketmakers.com>
2019-03-19 15:16:54 +0000

    linked admin panel to create new project page

--- admin_page/index.php
+++ admin_page/index.php
@@ -11,7 +11,7 @@ $query->execute();
 $projects = $query->fetchAll();
 
 echo '<h1>Admin Panel</h1>
-<div class="create">Create New<a href="#" class="add">+</a></div><table>
+<div class="create">Create New<a href="../add_new/add_project_page.php" class="add">+</a></div><table>
     <tr>
         <th>Number</th>
         <th>Project Title</th> 
