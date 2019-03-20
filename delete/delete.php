c0023054b6aff1779af2df0fa26698d2c2079f80
Alice <alice@rocketmakers.com>
2019-03-20 16:39:03 +0000

    created delete function doc

--- delete/delete.php
+++ delete/delete.php
@@ -1,1 +1,11 @@
 <?php
+$db = new PDO('mysql:host=192.168.20.20;dbname=cms_portfolio', 'root', '');
+$db-> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+echo '<link rel="stylesheet" type="text/css" href="../add_new/add_proj.css"><link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">';
+
+$deleteProject = 'DELETE `id`, `project_title`, `img_url`, `site_url` FROM projects WHERE `id` = ?;';
+$query = $db->prepare($deleteProject);
+$query->execute([$_GET['id']]);
+$project = $query->fetch();
+
