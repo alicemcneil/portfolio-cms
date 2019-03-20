acabee756c14d46a41c7019c868e07bfe3a63efc
Alice <alice@rocketmakers.com>
2019-03-20 19:01:11 +0000

    made delete function work

--- delete/delete.php
+++ delete/delete.php
@@ -1,11 +1,14 @@
 <?php
 $db = new PDO('mysql:host=192.168.20.20;dbname=cms_portfolio', 'root', '');
 $db-> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-echo '<link rel="stylesheet" type="text/css" href="../add_new/add_proj.css"><link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">';
-
-$deleteProject = 'DELETE `id`, `project_title`, `img_url`, `site_url` FROM projects WHERE `id` = ?;';
+?>
+<link rel="stylesheet" type="text/css" href="../add_new/add_proj.css"><link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+<?php
+$deleteProject = 'DELETE FROM `projects` WHERE `id` = ?;';
 $query = $db->prepare($deleteProject);
 $query->execute([$_GET['id']]);
 $project = $query->fetch();
+?>
+<h1>This project has been deleted from the Database</h1>
+<a class="button back" href="../admin_page/index.php">Back to Admin Panel</a>
 
