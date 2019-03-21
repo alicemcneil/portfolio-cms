f5c8a4dc688117848681029966eed19609283724
Alice <alice@rocketmakers.com>
2019-03-21 13:26:00 +0000

    password and username authentication (hashed)

--- login/login_func.php
+++ login/login_func.php
@@ -1,21 +1,21 @@
 <?php
 
 define('USERNAME', 'alice');
-define('PASSWORD', '1234');
+define('PASSWORD', '$2y$10$fMgFOggS3qcfO0GmqU.vo./Cpy3FLYWuk4.aFl/bNuPr3ZLz9BIKi'); //pw: 1234
 
 if (!empty($_POST['username']) && !empty($_POST['password'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
     session_start();
 
-    if ($username == USERNAME && $password == PASSWORD) {
+    if ($username == USERNAME && password_verify($password, PASSWORD)) {
         $_SESSION['loggedIn'] = true;
-
-        header('Location: ../admin_page/index.php');
+        header('Location: login_confirm.php');
     } else {
-        header('Location: index.php?login=false');
+        header('Location: login_form.php');
     }
-
-} else {
-    echo 'Incorrect username or password';
 }
+
+
+
+
